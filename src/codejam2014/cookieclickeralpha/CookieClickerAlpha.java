bd094b922913dd814c1c22990c8b7d0c34a70e71
sheehanj@lab126.com <sheehanj@lab126.com>
2014-05-09 14:37:03 -0700

    Added CookieClickerAlpha to practice on

---
+++ src/codejam2014/cookieclickeralpha/CookieClickerAlpha.java
@@ -0,0 +1,40 @@
+package codejam2014.cookieclickeralpha;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class CookieClickerAlpha {
+
+	Scanner sc = new Scanner(getClass().getResourceAsStream(IN));
+	static final String FILENAME = "A-large";
+	static final String IN = FILENAME + ".in";
+	static final String OUT = FILENAME + ".out";
+	PrintStream out = System.out;
+
+	private void solve(String string) {
+		System.out.println("Solving: " + string);
+		int ans = 0;
+		out.println(ans);
+		System.out.println("answer: " + ans);
+	}
+
+	private void run() throws Exception {
+		System.out.println("run()");
+		out = new PrintStream(new FileOutputStream(OUT));
+		int i = 0;
+		while(sc.hasNextLine()){
+			String nextLine = sc.nextLine();
+			solve(nextLine);
+			i++;
+		}
+		System.out.println("Total num lines: " + i);
+		sc.close();
+		out.close();
+	}
+
+	public static void main(String args[]) throws Exception {
+		new CookieClickerAlpha().run();
+	}
+
+}
