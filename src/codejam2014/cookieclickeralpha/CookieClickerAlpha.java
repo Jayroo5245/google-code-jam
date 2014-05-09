ec6af3501b84ab7e621c8efcef46a0a1e99fdd3e
sheehanj@lab126.com <sheehanj@lab126.com>
2014-05-09 15:37:58 -0700

    updated the Practice Template

--- src/codejam2014/cookieclickeralpha/CookieClickerAlpha.java
+++ src/codejam2014/cookieclickeralpha/CookieClickerAlpha.java
@@ -3,17 +3,20 @@ package codejam2014.cookieclickeralpha;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-
+//https://code.google.com/codejam/contest/2974486/dashboard#s=p1
 public class CookieClickerAlpha {
 
-	Scanner sc = new Scanner(getClass().getResourceAsStream(IN));
+	Scanner mScanner = new Scanner(getClass().getResourceAsStream(IN));
 	static final String FILENAME = "A-large";
 	static final String IN = FILENAME + ".in";
 	static final String OUT = FILENAME + ".out";
 	PrintStream out = System.out;
+	
+	static final int LENGTH = 3;
+	static final int WIDTH = 3;
 
-	private void solve(String string) {
-		System.out.println("Solving: " + string);
+	private void solve(float[][] matrix) {
+		System.out.println("Solving: ");
 		int ans = 0;
 		out.println(ans);
 		System.out.println("answer: " + ans);
@@ -22,19 +25,32 @@ public class CookieClickerAlpha {
 	private void run() throws Exception {
 		System.out.println("run()");
 		out = new PrintStream(new FileOutputStream(OUT));
-		int i = 0;
-		while(sc.hasNextLine()){
-			String nextLine = sc.nextLine();
-			solve(nextLine);
-			i++;
+		int numTestCases = mScanner.nextInt();
+		if (numTestCases < 1 || numTestCases > 100) {
+			System.out.println("numTestCases invalid" + numTestCases);
+			out.println("numTestCases invalid" + numTestCases);
+			return;
 		}
-		System.out.println("Total num lines: " + i);
-		sc.close();
+		for (int x = 0; x < numTestCases; x++) {
+			float floatMatrix[][] = fillMatrix();
+			solve(floatMatrix);
+		}
+		mScanner.close();
 		out.close();
 	}
 
 	public static void main(String args[]) throws Exception {
 		new CookieClickerAlpha().run();
+	}
+	
+	private float[][] fillMatrix() {
+		float intMatrix[][] = new float[LENGTH][WIDTH];
+		for (int row = 0; row < intMatrix.length; row++) {
+			for (int col = 0; col < intMatrix[row].length; col++) {
+				intMatrix[row][col] = mScanner.nextFloat();
+			}
+		}
+		return intMatrix;
 	}
 
 }
--- src/codejam2014/practice/PracticeTemplate.java
+++ src/codejam2014/practice/PracticeTemplate.java
@@ -5,15 +5,17 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class PracticeTemplate {
-
-	Scanner sc = new Scanner(getClass().getResourceAsStream(IN));
+	Scanner mScanner = new Scanner(getClass().getResourceAsStream(IN));
 	static final String FILENAME = "A-large";
 	static final String IN = FILENAME + ".in";
 	static final String OUT = FILENAME + ".out";
 	PrintStream out = System.out;
+	
+	static final int LENGTH = 3;
+	static final int WIDTH = 3;
 
-	private void solve(String string) {
-		System.out.println("Solving: " + string);
+	private void solve(float[][] matrix) {
+		System.out.println("Solving: ");
 		int ans = 0;
 		out.println(ans);
 		System.out.println("answer: " + ans);
@@ -22,19 +24,30 @@ public class PracticeTemplate {
 	private void run() throws Exception {
 		System.out.println("run()");
 		out = new PrintStream(new FileOutputStream(OUT));
-		int i = 0;
-		while(sc.hasNextLine()){
-			String nextLine = sc.nextLine();
-			solve(nextLine);
-			i++;
+		int numTestCases = mScanner.nextInt();
+		if (numTestCases < 1 || numTestCases > 100) {
+			System.out.println("numTestCases invalid" + numTestCases);
+			out.println("numTestCases invalid" + numTestCases);
+			return;
 		}
-		System.out.println("Total num lines: " + i);
-		sc.close();
+		for (int x = 0; x < numTestCases; x++) {
+			float floatMatrix[][] = fillMatrix();
+			solve(floatMatrix);
+		}
+		mScanner.close();
 		out.close();
 	}
 
 	public static void main(String args[]) throws Exception {
 		new PracticeTemplate().run();
 	}
-
+	private float[][] fillMatrix() {
+		float intMatrix[][] = new float[LENGTH][WIDTH];
+		for (int row = 0; row < intMatrix.length; row++) {
+			for (int col = 0; col < intMatrix[row].length; col++) {
+				intMatrix[row][col] = mScanner.nextFloat();
+			}
+		}
+		return intMatrix;
+	}
 }
